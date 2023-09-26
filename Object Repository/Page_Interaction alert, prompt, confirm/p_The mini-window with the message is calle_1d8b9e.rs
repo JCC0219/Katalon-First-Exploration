8e5d6265bb9af38b30a9fb277def8f5de5448e62
jichiew <jichiew@MYDC5CG3193LLT.atrapa.deloitte.com>
2023-09-26 21:16:36 +0800

    add automation for alert,popup,frame, and mouse hover

---
+++ Object Repository/Page_Interaction alert, prompt, confirm/p_The mini-window with the message is calle_1d8b9e.rs
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_The mini-window with the message is calle_1d8b9e</name>
+   <tag></tag>
+   <elementGuidId>96f6c4aa-76a1-4bf7-a5bb-680c76dd6006</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>p:nth-of-type(4)</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>9bb1dd01-5c07-4032-ae8e-ca49ee38ba86</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>The mini-window with the message is called a modal window. The word “modal” means that the visitor can’t interact with the rest of the page, press other buttons, etc, until they have dealt with the window. In this case – until they press “OK”.</value>
+      <webElementGuid>29a06fdc-dc93-4f1a-ae4d-150d74044760</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;working-hover&quot;]/body[1]/div[@class=&quot;page-wrapper page-wrapper_sidebar_on&quot;]/div[@class=&quot;page page_sidebar_on page_inner_padding page_sidebar-animation-on&quot;]/div[@class=&quot;page__inner&quot;]/main[@class=&quot;main main_width-limit&quot;]/div[@class=&quot;content&quot;]/article[@class=&quot;formatted&quot;]/div[2]/p[4]</value>
+      <webElementGuid>3cbe237e-be33-431e-bbc8-0ed807582677</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=';'])[1]/following::p[1]</value>
+      <webElementGuid>ea207a1b-0a8f-46f1-887d-98d3ac610b7c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=')'])[1]/following::p[1]</value>
+      <webElementGuid>35f482e5-cb24-407b-8441-94cfb8ee78d8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='prompt'])[2]/preceding::p[1]</value>
+      <webElementGuid>a32f410a-2ef3-4b85-ab07-e7380167e3db</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='The mini-window with the message is called a']/parent::*</value>
+      <webElementGuid>d228d034-8a47-4313-86df-4cf5c79fe5dc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//p[4]</value>
+      <webElementGuid>05e27876-960f-46ac-889b-924ee2535890</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = 'The mini-window with the message is called a modal window. The word “modal” means that the visitor can’t interact with the rest of the page, press other buttons, etc, until they have dealt with the window. In this case – until they press “OK”.' or . = 'The mini-window with the message is called a modal window. The word “modal” means that the visitor can’t interact with the rest of the page, press other buttons, etc, until they have dealt with the window. In this case – until they press “OK”.')]</value>
+      <webElementGuid>1db613b8-5975-4828-8f22-5814b768816c</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
